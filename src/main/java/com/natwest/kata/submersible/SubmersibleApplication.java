23a334c19a292ad5fd0d9ca85422b1aa33dd1e58
Suman Pattnaik <sumanp@maveric-systems.com>
2025-11-20 13:01:24 +0530

    Initial commit Spring Boot project setup

---
+++ src/main/java/com/natwest/kata/submersible/SubmersibleApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.kata.submersible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmersibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmersibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natwest/kata/submersible/SubmersibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.kata.submersible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmersibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
