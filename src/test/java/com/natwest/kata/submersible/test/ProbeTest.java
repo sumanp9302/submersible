dc9587b826ecd3c9f142171ba501842de8bab5c7
sumanp <sumanprofile9302@gmail.com>
2025-11-20 14:28:16 +0530

    Add failing test for moveBackward() in 3D grid

--- src/test/java/com/natwest/kata/submersible/test/ProbeTest.java
+++ src/test/java/com/natwest/kata/submersible/test/ProbeTest.java
@@ -27,4 +27,14 @@ public class ProbeTest {
         assertEquals(4, probe.getY());
         assertEquals(5, probe.getZ());
     }
+
+    @Test
+    void shouldMoveBackwardIn3DGridBasedOnDirection()
+    {
+        Probe probe = new Probe(2,3,5, Direction.NORTH);
+        probe.moveBackward();
+        assertEquals(2, probe.getX());
+        assertEquals(3, probe.getY());
+        assertEquals(5, probe.getZ());
+    }
 }
